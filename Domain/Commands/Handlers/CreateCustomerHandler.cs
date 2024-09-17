e4999bceb336853febc03852aa9155f02bc74370
gnardydev <gacnardy@gmail.com>
2024-09-17 17:03:51 -0300

    shop

---
+++ Domain/Commands/Handlers/CreateCustomerHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Shop.Domain.Commands.Requests;
+using Shop.Domain.Commands.Responses;
+
+namespace Shop.Domain.Commands.Handlers
+{
+    public class CreateCustomerHandler : 
+        IRequestHandler<CreateCustomerRequest,CreateCustomerResponse >  //recebe, retorna
+    {
+        public Task<CreateCustomerResponse> Handle(CreateCustomerRequest request, CancellationToken cancellationToken)
+        {
+            //  verificar se cliente está cadastrado
+            // valida os dados
+            // insere cliente
+            // envia email de boas vindas
+            var result = new CreateCustomerResponse
+            {
+                Id = Guid.NewGuid(),
+                Name = "Gabriel dev2",
+                Email = "dev2@wegen.com.br",
+                Date = DateTime.Now,
+            };
+            return Task.FromResult(result);
+        }
+    }
+}
